8402ad3c2c674d7a950124b2416f3d77a6e773c8
Jhonael <jhonaelrocha@gmail.com>
2023-07-29 12:03:36 -0300

    Update Andares13o

--- Andares13o.py
+++ Andares13o.py
@@ -1,8 +1,10 @@
+print("Andares : ")
 for i in range(1,21):
     if i == 13:
         continue
     print("Andar : " + str(i))
-for i in range(21,1,-1):
+print("Andares ao contrário : ")
+for i in range(20,0,-1):
     if i == 13:
         continue
     print("Andar : " + str(i))
